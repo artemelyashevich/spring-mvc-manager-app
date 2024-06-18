f989ed999f0c7d915e6a98dddf93a4c53266d110
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-06-18 18:46:03 +0300

    add: validation

--- src/main/java/com/example/manager/controller/ProductsController.java
+++ src/main/java/com/example/manager/controller/ProductsController.java
@@ -3,9 +3,12 @@ package com.example.manager.controller;
 import com.example.manager.controller.paylaod.NewProductPayload;
 import com.example.manager.entity.Product;
 import com.example.manager.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +32,17 @@ public class ProductsController {
     }
 
     @PostMapping("/create")
-    public String createProduct(NewProductPayload newProductPayload) {
-        Product product = this.productService.createProduct(newProductPayload.title(), newProductPayload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayload newProductPayload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).toList());
+            model.addAttribute("payload", newProductPayload);
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(newProductPayload.title(), newProductPayload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 }
--- src/main/java/com/example/manager/controller/paylaod/NewProductPayload.java
+++ src/main/java/com/example/manager/controller/paylaod/NewProductPayload.java
@@ -1,7 +1,14 @@
 package com.example.manager.controller.paylaod;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record NewProductPayload(
+        @NotNull()
+        @Size(min = 3, max = 50)
         String title,
+        @NotNull()
+        @Size(max = 1000)
         String details
 ) {
 }
